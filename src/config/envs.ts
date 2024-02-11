908e1e95b712ba948694f2640c72b9c1fefcac33
Anghelo Alva <ance.anghelo@gmail.com>
2024-02-11 19:30:14 -0300

    Remove NODE_ENV variable from envs.ts

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,7 +3,6 @@ import { get } from 'env-var'
 
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
-  NODE_ENV: get('NODE_ENV').required().asString(),
   CLIENT_URL: get('CLIENT_URL').required().asString(),
   DATABASE_URL: get('DATABASE_URL').required().asString(),
   STRIPE_PUBLIC_KEY: get('STRIPE_PUBLIC_KEY').required().asString(),
